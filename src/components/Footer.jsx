e5f1996021e87719f1783a7e16f59bc1b1ba1433
joakimlees <91544170+joakimlees@users.noreply.github.com>
2023-05-04 14:29:12 +0200

    made footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,3 +1,24 @@
+import { Link } from "react-router-dom";
+import logoLight from "../build/logo-light.svg";
+
 export function Footer() {
-  return <footer className="flex">Footer component ss</footer>;
+  return (
+    <footer className="bg-primary py-14">
+      <div className="mx-auto max-w-screen-2xl px-3 sm:px-5">
+        <div className="mb-10 w-20 sm:w-24">
+          <Link to="/">
+            <img src={logoLight} alt="holidaze logo" />
+          </Link>
+        </div>
+        <ul className="text-base text-white">
+          <li className="my-2">
+            <Link to="/about">About</Link>
+          </li>
+          <li>
+            <Link to="/contact">Contact</Link>
+          </li>
+        </ul>
+      </div>
+    </footer>
+  );
 }
