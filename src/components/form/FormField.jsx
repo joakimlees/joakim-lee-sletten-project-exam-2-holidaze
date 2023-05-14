37011c49751e999c528e8518403516c4fb860660
joakimlees <91544170+joakimlees@users.noreply.github.com>
2023-05-14 17:36:30 +0200

    added placeholder to form fields input

--- src/components/form/FormField.jsx
+++ src/components/form/FormField.jsx
@@ -4,7 +4,7 @@ export function FormField(props) {
       <label className="font-headings text-dark text-sm" for={props.labelFor}>
         {props.labelContent}
       </label>
-      <input name={props.inputName} type={props.inputType} required={props.inputRequired} pattern={props.inputPattern} title={props.inputTitle} className="text-black border font-paragraph text-sm rounded-lg border-dark text-sm px-3" />
+      <input name={props.inputName} type={props.inputType} required={props.inputRequired} pattern={props.inputPattern} title={props.inputTitle} placeholder={props.inputPlaceholder} className="text-black border font-paragraph text-sm rounded-lg border-dark text-sm px-2" />
     </div>
   );
 }
--- src/components/pages/profile/Register/index.jsx
+++ src/components/pages/profile/Register/index.jsx
@@ -10,10 +10,10 @@ export function Register() {
       <div className="mx-auto max-w-screen-2xl px-3 sm:px-5">
         <h1 className="text-center mt-20 font-headings text-2xl text-dark">Register</h1>
         <form className="bg-light py-16 px-10 rounded-lg max-w-xs mx-auto mt-10 mb-20" action="#">
-          <FormField labelContent="Username" labelFor="username" inputName="username" inputType="text" inputRequired={true} />
-          <FormField labelContent="Email" labelFor="email" inputName="email" inputType="email" inputRequired={true} />
-          <FormField labelContent="Avatar" labelFor="avatar" inputName="avatar" inputType="url" inputRequired={false} />
-          <FormField labelContent="Password" labelFor="password" inputName="password" inputType="text" inputRequired={true} />
+          <FormField labelContent="Username" labelFor="username" inputName="username" inputType="text" inputPlaceholder="dinDjarin123" inputRequired={true} />
+          <FormField labelContent="Email" labelFor="email" inputName="email" inputType="email" inputPlaceholder="example@stud.noroff.no" inputRequired={true} />
+          <FormField labelContent="Avatar" labelFor="avatar" inputName="avatar" inputType="url" inputPlaceholder="imageUrl.no" inputRequired={false} />
+          <FormField labelContent="Password" labelFor="password" inputName="password" inputType="password" inputPlaceholder="Choose a password..." inputRequired={true} />
           <div className="flex justify-between mb-8">
             <FormField labelContent="Customer" labelFor="roleCustomer" inputName="role" inputType="radio" inputRequired={false} />
             <FormField labelContent="Manager" labelFor="roleManager" inputName="role" inputType="radio" inputRequired={false} />
