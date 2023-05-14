b9638ca10a95cbb144385c0912e8996b97b252e4
joakimlees <91544170+joakimlees@users.noreply.github.com>
2023-05-14 19:58:09 +0200

    started on useForm and yup

--- src/components/form/FormField.jsx
+++ src/components/form/FormField.jsx
@@ -1,3 +1,19 @@
+import { useForm } from "react-hook-form";
+
+export function FormField({ inputName, inputTitle, labelFor, labelContent, inputPattern, inputRequired, inputType, inputPlaceholder }) {
+  const { register } = useForm();
+
+  return (
+    <div className="flex flex-col my-2">
+      <label className="font-headings text-dark text-sm" for={labelFor}>
+        {labelContent}
+      </label>
+      <input {...register(inputName)} name={inputName} type={inputType} required={inputRequired} pattern={inputPattern} title={inputTitle} placeholder={inputPlaceholder} className="text-black border font-paragraph text-sm rounded-lg border-dark text-sm px-2" />
+    </div>
+  );
+}
+
+/*
 export function FormField(props) {
   return (
     <div className="flex flex-col my-2">
@@ -8,3 +24,4 @@ export function FormField(props) {
     </div>
   );
 }
+*/
