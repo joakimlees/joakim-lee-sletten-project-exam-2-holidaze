ca78a9ad4effc6055eded9653291910f66758347
joakimlees <91544170+joakimlees@users.noreply.github.com>
2023-05-17 01:04:29 +0200

    made register user work. still got to cleane up

--- src/components/form/schema/userSchema.js
+++ src/components/form/schema/userSchema.js
@@ -2,10 +2,10 @@ import * as yup from "yup";
 
 export const userSchema = yup
   .object({
-    username: yup.string().min(3, "Your username should be at least 3 characters.").max(12, "Your username cannot be longer than 12 characters.").required("Please enter a username"),
+    name: yup.string().min(3, "Your username should be at least 3 characters.").max(12, "Your username cannot be longer than 12 characters.").required("Please enter a username"),
     email: yup.string().email("You must enter a valid email address").required("Please enter your email address"),
     avatar: yup.string().url("avatar must be a valid url").optional(),
     password: yup.string().min(5, "Your password should be at least 5 characters.").max(15, "Your password cannot be longer than 15 characters.").required("Please enter a password"),
-    role: yup.string().required("Please select if you intend to use holidaze as a customer og as a manager"),
+    venueManager: yup.boolean().optional().oneOf([true, false], "Please select if you intend to use Holidaze as a customer or as a manager"),
   })
   .required();
--- src/components/pages/profile/Register/index.jsx
+++ src/components/pages/profile/Register/index.jsx
@@ -1,13 +1,11 @@
 import { FormField } from "../../../form/FormField";
 import { ButtonPrimary } from "../../../ui/actions/buttons/ButtonPrimary";
 import { useForm } from "react-hook-form";
-import { useState } from "react";
 import { userSchema } from "../../../form/schema/userSchema";
 import { yupResolver } from "@hookform/resolvers/yup";
+import { API_HOLIDAZE_URL } from "../../../../api/constants";
 
 export function Register() {
-  const [error, setError] = useState(null);
-
   const {
     handleSubmit,
     register,
@@ -16,9 +14,11 @@ export function Register() {
     resolver: yupResolver(userSchema),
   });
 
-  async function onSubmit(data) {
+  const onSubmit = async data => {
+    const registerUrl = API_HOLIDAZE_URL + "/auth/register";
+
     try {
-      const response = await fetch("API_ENDPOINT_URL", {
+      const response = await fetch(registerUrl, {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
@@ -26,28 +26,25 @@ export function Register() {
         body: JSON.stringify(data),
       });
 
-      if (!response.ok) {
-        throw new Error("Registration failed");
+      if (response.ok) {
+        // Request succeeded
+        console.log("User registered successfully");
+      } else {
+        // Request failed
+        console.error("Failed to register user");
       }
-
-      // Registration successful
-      console.log("User registered successfully");
     } catch (error) {
-      setError(error.message);
+      // Request error
+      console.error("Error registering user:", error);
     }
-  }
-
-  /*
-  function onSubmit(data) {
-    console.log(data);
-  } */
+  };
 
   return (
     <main className="grow">
       <div className="mx-auto max-w-screen-2xl px-3 sm:px-5">
         <h1 className="text-center mt-20 font-headings text-2xl text-dark">Register</h1>
         <form className="bg-light py-16 px-10 rounded-lg max-w-xs mx-auto mt-10 mb-20" onSubmit={handleSubmit(onSubmit)}>
-          <FormField register={register} labelText="Username" htmlFor="username" name="username" type="text" placeholder="dinDjarin123" required={true} errorText={errors.username?.message} />
+          <FormField register={register} labelText="Username" htmlFor="name" name="name" type="text" placeholder="dinDjarin123" required={true} errorText={errors.name?.message} />
 
           <FormField register={register} labelText="Email" htmlFor="email" name="email" type="email" placeholder="example@stud.noroff.no" required={true} errorText={errors.email?.message} />
 
@@ -56,9 +53,9 @@ export function Register() {
           <FormField register={register} errorText={errors.password?.message} labelText="Password" htmlFor="password" name="password" type="password" placeholder="Choose a password..." required={true} />
 
           <div className="flex justify-between mb-8">
-            <FormField register={register} errorText={errors.role?.message} labelText="Customer" htmlFor="roleCustomer" name="role" value="Customer" type="radio" required={true} />
+            <FormField register={register} errorText={errors.role?.message} labelText="Customer" htmlFor="roleCustomer" name="venueManager" value={false} type="radio" required={true} />
 
-            <FormField register={register} errorText={errors.role?.message} labelText="Manager" htmlFor="roleManager" name="role" value="Manager" type="radio" required={true} />
+            <FormField register={register} errorText={errors.venueManager?.message} labelText="Manager" htmlFor="roleManager" name="venueManager" value={true} type="radio" required={true} />
           </div>
 
           <ButtonPrimary type="submit" innerContent="Register now" />
