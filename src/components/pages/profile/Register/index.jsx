8ff46fff6c6d59cf0c7c9b8852ac90d1114d3b1e
joakimlees <91544170+joakimlees@users.noreply.github.com>
2023-05-18 00:15:52 +0200

    successfully manage to target method and action on form

--- src/components/pages/profile/Register/index.jsx
+++ src/components/pages/profile/Register/index.jsx
@@ -3,11 +3,14 @@ import { ButtonPrimary } from "../../../ui/actions/buttons/ButtonPrimary";
 import { useForm } from "react-hook-form";
 import { userSchema } from "../../../form/schema/userSchema";
 import { yupResolver } from "@hookform/resolvers/yup";
-import { API_HOLIDAZE_URL, AUTH_REGISTER } from "../../../../api/constants";
+import { useRef } from "react";
+//import { API_HOLIDAZE_URL, AUTH_REGISTER } from "../../../../api/constants";
 
-const url = API_HOLIDAZE_URL + AUTH_REGISTER;
+//const url = API_HOLIDAZE_URL + AUTH_REGISTER;
 
 export function Register() {
+  const formRef = useRef();
+
   const {
     handleSubmit,
     register,
@@ -16,6 +19,16 @@ export function Register() {
     resolver: yupResolver(userSchema),
   });
 
+  function onSubmit(data, event) {
+    const form = event.target;
+    const url = new URL(form.action);
+    const actionPath = url.pathname;
+    const method = form.method;
+
+    console.log(actionPath + method);
+  }
+
+  /*
   const onSubmit = async data => {
     try {
       const response = await fetch(url, {
@@ -38,12 +51,13 @@ export function Register() {
       console.error("Error registering user:", error);
     }
   };
+  */
 
   return (
     <main className="grow">
       <div className="mx-auto max-w-screen-2xl px-3 sm:px-5">
         <h1 className="text-center mt-20 font-headings text-2xl text-dark">Register</h1>
-        <form className="bg-light py-16 px-10 rounded-lg max-w-xs mx-auto mt-10 mb-20" onSubmit={handleSubmit(onSubmit)}>
+        <form className="bg-light py-16 px-10 rounded-lg max-w-xs mx-auto mt-10 mb-20" method={"post"} action={"/auth/register"} onSubmit={handleSubmit(onSubmit)}>
           <FormField register={register} labelText="Username" htmlFor="name" name="name" type="text" placeholder="dinDjarin123" required={true} errorText={errors.name?.message} />
 
           <FormField register={register} labelText="Email" htmlFor="email" name="email" type="email" placeholder="example@stud.noroff.no" required={true} errorText={errors.email?.message} />
