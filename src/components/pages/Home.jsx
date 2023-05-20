9a1e224be6ccd198c1b884c066c785e90e80521d
joakimlees <91544170+joakimlees@users.noreply.github.com>
2023-05-20 22:15:17 +0200

    started on function to find eraliest date

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -17,7 +17,7 @@ export function Home() {
         const response = await fetch(url);
         const data = await response.json();
         setData(data);
-        console.log(data);
+        return data;
       } catch (error) {
         setError(true);
       } finally {
@@ -26,6 +26,37 @@ export function Home() {
     }
     getData();
   }, [url]);
+
+  function getEarliestDate(bookings) {
+    const currentDate = new Date();
+
+    const unavailableDates = bookings.reduce((unavailable, booking) => {
+      const fromDate = new Date(booking.dateFrom);
+      const toDate = new Date(booking.dateTo);
+      const datesInRange = [];
+
+      for (let date = fromDate; date <= toDate; date.setDate(date.getDate() + 1)) {
+        datesInRange.push(new Date(date));
+      }
+
+      return [...unavailable, ...datesInRange];
+    }, []);
+
+    const sortedUnavailableDates = unavailableDates.sort((a, b) => a - b);
+
+    for (let i = 0; i < sortedUnavailableDates.length; i++) {
+      if (sortedUnavailableDates[i] > currentDate) {
+        return currentDate;
+      } else if (i + 1 < sortedUnavailableDates.length && currentDate < sortedUnavailableDates[i + 1]) {
+        return sortedUnavailableDates[i];
+      }
+    }
+
+    return sortedUnavailableDates[sortedUnavailableDates.length - 1];
+  }
+
+  const earliestDate = getEarliestDate(data[0].bookings);
+  console.log("this: date: " + earliestDate);
 
   if (loading) {
     return <div className="loading-fetch">loading..................</div>;
