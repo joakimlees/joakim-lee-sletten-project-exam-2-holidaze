3a70ea189cae40f2f1c86f9899075d9288d98880
joakimlees <91544170+joakimlees@users.noreply.github.com>
2023-05-20 15:07:09 +0200

    working on styling

--- src/components/VenueCard.jsx
+++ src/components/VenueCard.jsx
@@ -1,7 +1,12 @@
-export function VenueCard({ venue: { name } }) {
+export function VenueCard({ venue: { name, media } }) {
   return (
-    <div>
-      <li>{name}</li>
-    </div>
+    <li className="b-light py-4 px-4 rounded-lg border border-dark">
+      <div className="max-w-xs">
+        <img src={media[0]} alt="" />
+      </div>
+      <article>
+        <h3 className="font-headings text-primary">{name}</h3>
+      </article>
+    </li>
   );
 }
