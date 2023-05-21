f5a4a8701443b7dbdd23e6102a2fd9d4edcc8a46
joakimlees <91544170+joakimlees@users.noreply.github.com>
2023-05-21 22:33:17 +0200

    made breadcrumbs functionality

--- src/components/BreadCrumbs.jsx
+++ src/components/BreadCrumbs.jsx
@@ -2,25 +2,26 @@ import { Link, useLocation } from "react-router-dom";
 
 export function BreadCrumbs() {
   const location = useLocation();
-  console.log(location);
+
+  let currentLink = "";
+
+  const crumbs = location.pathname
+    .split("/")
+    .filter(crumb => crumb !== "")
+    .map(crumb => {
+      currentLink = +`/${crumb}`;
+
+      return (
+        <li className="" key={crumb}>
+          <Link to={currentLink}>{"| " + crumb}</Link>
+        </li>
+      );
+    });
 
   return (
     <nav>
       <div className="mx-auto max-w-screen-2xl px-3 sm:px-5">
-        <ul className="flex gap-2">
-          <li>
-            <Link>Hello</Link>
-          </li>
-          <li>
-            <Link>Hello</Link>
-          </li>
-          <li>
-            <Link>Hello</Link>
-          </li>
-          <li>
-            <Link>Hello</Link>
-          </li>
-        </ul>
+        <ul className="flex gap-2">{crumbs}</ul>
       </div>
     </nav>
   );
