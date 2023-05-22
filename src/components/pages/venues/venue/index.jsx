58e001023a62b0707adca4db535adcc4ea3007c1
joakimlees <91544170+joakimlees@users.noreply.github.com>
2023-05-22 20:46:54 +0200

    working on venue with id component

--- src/components/pages/venues/venue/index.jsx
+++ src/components/pages/venues/venue/index.jsx
@@ -28,10 +28,35 @@ export function Venue() {
 
   return (
     <main className="grow">
-      <div className="bg-secondary mx-auto max-w-screen-2xl px-3 sm:px-5">
-        <h1>{name}</h1>
-        <div>
-          <img src={media} alt="" />
+      <div className="mx-auto max-w-screen-2xl px-3 sm:px-5">
+        <div className="mx-auto px-10 max-w-3xl bg-secondary">
+          <h1>{name}</h1>
+          <div className="w-full w-full h-80 border-dark border-2">
+            <img src={media} alt="" className="h-full w-full object-cover" />
+          </div>
+          <dl className="my-8">
+            <div className="flex justify-between mb-4">
+              <div>
+                <dt className="text-dark font-bold">Price</dt>
+                <dd>{price + ",-"}</dd>
+              </div>
+              <div className="text-right ms-8">
+                <dt className="text-dark font-bold">Country</dt>
+                <dd>{country}</dd>
+              </div>
+            </div>
+            <div className="flex justify-between">
+              <div>
+                <dt className="text-dark font-bold">Guest capacity</dt>
+                <dd>{maxGuests}</dd>
+              </div>
+              <div className="text-right ms-8">
+                <dt className="text-dark font-bold">Owner</dt>
+                <dd className="break-all">{ownerName}</dd>
+                <dd className="break-all">{email}</dd>
+              </div>
+            </div>
+          </dl>
         </div>
       </div>
     </main>
