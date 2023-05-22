701eb4b44de621e3501648be3fbd6e9da0ca8cb9
joakimlees <91544170+joakimlees@users.noreply.github.com>
2023-05-22 20:34:20 +0200

    working on venue by id

--- src/components/pages/venues/venue/index.jsx
+++ src/components/pages/venues/venue/index.jsx
@@ -6,7 +6,34 @@ export function Venue() {
   const { id } = useParams();
   const url = `${API_HOLIDAZE_VENUE_URL}/${id}/${BOOKINGS_OWNER}`;
   const { data, loading, error } = useFetch(url);
-  const { name, price } = data;
+  const {
+    name,
+    media,
+    price,
+    created,
+    description,
+    maxGuests,
+    location: { address, continent, country, city, zip },
+    owner: { name: ownerName, email },
+    bookings: { dateFrom, dateTo },
+  } = data;
 
-  return <div>{name}</div>;
+  if (loading) {
+    return <div className="loading-fetch">loading..................</div>;
+  }
+
+  if (error) {
+    return <div className="error-fetch">Sorry.. something went wrong. try to reload the site or try again later</div>;
+  }
+
+  return (
+    <main className="grow">
+      <div className="bg-secondary mx-auto max-w-screen-2xl px-3 sm:px-5">
+        <h1>{name}</h1>
+        <div>
+          <img src={media} alt="" />
+        </div>
+      </div>
+    </main>
+  );
 }
