e5a330275ee11af7da2a3a1c98b7dbfc928be779
joakimlees <91544170+joakimlees@users.noreply.github.com>
2023-05-22 13:49:55 +0200

    made some file structure

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import "./App.css";
 import React from "react";
-import * as page from "./components/pages/index";
+import { Home } from "./components/pages";
+import { Profile } from "./components/pages/profile";
+import { Venues } from "./components/pages/venues";
 import { Register } from "./components/pages/profile/Register";
 import { Login } from "./components/pages/profile/login";
 import { Routes, Route } from "react-router-dom";
@@ -11,8 +13,9 @@ function App() {
     <div className="h-full">
       <Routes>
         <Route path="/" element={<Layout />}>
-          <Route index element={<page.Home />} />
-          <Route path="venues" element={<page.Venues />} />
+          <Route index element={<Home />} />
+          <Route path="venues" element={<Venues />} />
+          <Route path="profile" element={<Profile />} />
           <Route path="profile/register" element={<Register />} />
           <Route path="profile/login" element={<Login />} />
         </Route>
--- src/components/pages/index.jsx
+++ src/components/pages/index.jsx
@@ -1,2 +1,50 @@
-export * from "./Home";
-export * from "./Venues";
+import { useState, useEffect } from "react";
+import { API_HOLIDAZE_URL, VENUES } from "../../api/constants";
+import { VenueCard } from "../VenueCard";
+
+export function Home() {
+  const url = API_HOLIDAZE_URL + VENUES + "?_owner=true&_bookings=true";
+
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
+
+  useEffect(() => {
+    async function getData() {
+      try {
+        setLoading(true);
+        setError(false);
+        const response = await fetch(url);
+        const data = await response.json();
+        setData(data);
+        return data;
+      } catch (error) {
+        setError(true);
+      } finally {
+        setLoading(false);
+      }
+    }
+    getData();
+  }, [url]);
+
+  if (loading) {
+    return <div className="loading-fetch">loading..................</div>;
+  }
+
+  if (error) {
+    return <div className="error-fetch">Sorry.. something went wrong. try to reload the site or try again later</div>;
+  }
+
+  return (
+    <main className="grow">
+      <div className="mx-auto max-w-screen-2xl px-3 sm:px-5">
+        <h1 className="font-headings font-bold text-xl text-center text-primary my-10">Home page</h1>
+        <ul className="flex flex-wrap justify-center gap-5">
+          {data.map(venue => (
+            <VenueCard key={venue.id} venue={venue} />
+          ))}
+        </ul>
+      </div>
+    </main>
+  );
+}
