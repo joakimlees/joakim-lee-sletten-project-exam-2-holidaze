947f4b5cf12fa1fc8549f974637f1af2501fa005
joakimlees <91544170+joakimlees@users.noreply.github.com>
2023-05-23 19:50:01 +0200

    working on calender

--- src/components/Calender.jsx
+++ src/components/Calender.jsx
@@ -3,7 +3,7 @@ import Calendar from "react-calendar";
 export function Calender() {
   return (
     <div>
-      <Calendar className="w-full h-full border p-2" minDate={new Date()} view="month" />
+      <Calendar className="w-full h-full border p-10 mt-10 mb-10" minDate={new Date()} view="month" />
     </div>
   );
 }
