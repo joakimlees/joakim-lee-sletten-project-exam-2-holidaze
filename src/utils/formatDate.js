27e26506ded78d61e092e5b5d4eeafaa39fd9b15
joakimlees <91544170+joakimlees@users.noreply.github.com>
2023-05-25 17:40:40 +0200

    js docs to formatt date function

--- src/utils/formatDate.js
+++ src/utils/formatDate.js
@@ -1,4 +1,8 @@
-//function to format date from for example new date() to be formatted as mm/dd/yy
+/**
+ * function to format date from for example new date() to be formatted as mm/dd/yy
+ * @param {Date} date - The date to be formatted.
+ * @returns {string} - The formatted date as a string.
+ */
 export function formatDate(date) {
   if (!date) {
     return ""; // Return empty string if date is null
