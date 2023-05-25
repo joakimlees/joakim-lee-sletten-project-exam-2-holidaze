9a98c2e7fcc68528779478a68c4fea9060c01e98
joakimlees <91544170+joakimlees@users.noreply.github.com>
2023-05-25 17:34:44 +0200

    jsdocs to getMaxDate

--- src/components/calendar/utils/getMaxDate.js
+++ src/components/calendar/utils/getMaxDate.js
@@ -1,9 +1,13 @@
-//getting a date in x amount of months later then current date.
+/**
+ * getting a date in x amount of months later then current date.
+ * @param {number} numberOfMonths - The number of months to add to the current date.
+ * @returns {Date} - The calculated max date.
+ */
 export function getMaxDate(numberOfMonths) {
   // Get the current date
   const currentDate = new Date();
 
-  // Calculate the maxDate by adding numberOfMonths("number") months to the current date
+  // Calculate the maxDate by adding numberOfMonths("number") months to the current date.
   const maxDate = new Date();
   maxDate.setMonth(currentDate.getMonth() + numberOfMonths);
 
