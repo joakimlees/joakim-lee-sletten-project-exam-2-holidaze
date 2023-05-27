d14628d588237c5d49dc8838249c19df2bbd9e98
joakimlees <91544170+joakimlees@users.noreply.github.com>
2023-05-27 21:52:43 +0200

    started on edit profile

--- src/components/pages/profile/bookings/edit/index.jsx
+++ src/components/pages/profile/bookings/edit/index.jsx
@@ -1,3 +1,9 @@
 export function EditUserBookings() {
-  return <div>Edit user bookings</div>;
+  return (
+    <main className="grow">
+      <div className="mx-auto max-w-screen-2xl px-3 sm:px-5">
+        <h1 className="text-center mt-20 font-headings text-2xl text-dark">Edit booking</h1>
+      </div>
+    </main>
+  );
 }
