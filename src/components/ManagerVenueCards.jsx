3d93fbc7183c1c9f8bf90673b04dcf2e8a07fdeb
joakimlees <91544170+joakimlees@users.noreply.github.com>
2023-05-28 19:11:51 +0200

    made venueManager page

---
+++ src/components/ManagerVenueCards.jsx
@@ -0,0 +1,56 @@
+import { formatDate } from "../utils/formatDate";
+import { Link } from "react-router-dom";
+import { useAuthPost } from "../api/auth/useAuthPost";
+import { API_HOLIDAZE_URL } from "../api/constants";
+import { useState } from "react";
+
+export function ManagerVenueCard({ venue: { name, id, created, updated, media } }) {
+  const [removeText, setRemoveText] = useState();
+  const createdDate = formatDate(created);
+  const updatedDate = formatDate(updated);
+
+  const { loading, postWithAuth } = useAuthPost();
+
+  const handleDeleteVenue = async () => {
+    const url = API_HOLIDAZE_URL + "/venues/" + id;
+    const method = "delete";
+
+    if (loading) {
+      setRemoveText("Loading...");
+    }
+
+    setRemoveText("Your venue was deleted. Refresh to see updated the venue manager");
+
+    await postWithAuth(url, {
+      method,
+    });
+  };
+
+  return (
+    <li className="flex-col bg-light w-80 py-4 px-4 rounded-lg gap-5 sm:flex sm:flex-row sm:w-full sm:max-w-full">
+      <div className="h-52 sm:h-full w-full sm:w-52 border-dark border-2 rounded-lg mx-auto">
+        <img src={media[0]} alt={name} className="h-full w-full object-cover rounded-lg" />
+      </div>
+      <article className="w-full grow">
+        <h2 className="font-headings font-semibold text-base text-primary mb-2">{name}</h2>
+        <dl>
+          <div>
+            <dt>Created</dt>
+            <dd className="font-semibold">{createdDate}</dd>
+            <dt>Updated</dt>
+            <dd className="font-semibold">{updatedDate}</dd>
+          </div>
+        </dl>
+        <div className="flex justify-center mt-6 py-2 bg-dark text-white rounded-lg">
+          <Link className="w-full h-full text-center">Edit booking</Link>
+        </div>
+        <div className="flex justify-center mt-6 py-2 bg-secondary text-white rounded-lg">
+          <button className="w-full h-full text-center" onClick={handleDeleteVenue}>
+            Delete venue
+          </button>
+        </div>
+        <div className="font-headings text-center text-base text-dark mb-2 mt-8">{removeText}</div>
+      </article>
+    </li>
+  );
+}
--- src/components/pages/profile/manager/index.jsx
+++ src/components/pages/profile/manager/index.jsx
@@ -1,9 +1,44 @@
+import { useAuthFetch } from "../../../../api/auth/useAuthFetch";
+import { API_HOLIDAZE_URL } from "../../../../api/constants";
+import { useLocalStorage } from "../../../../hooks/useLocalStorage";
+import { ManagerVenueCard } from "../../../ManagerVenueCards";
+
 export function ManagerVenues() {
+  const [profile] = useLocalStorage("profile");
+
+  const method = "get";
+
+  const name = profile.name;
+
+  const url = `${API_HOLIDAZE_URL}/profiles/${name}/venues`;
+
+  const { data, loading, error } = useAuthFetch(url, {
+    method,
+  });
+
+  console.log(data);
+
+  if (!data) {
+    return <div className="loading-fetch">loading..................</div>;
+  }
+
+  if (loading) {
+    return <div className="loading-fetch">loading..................</div>;
+  }
+
+  if (error) {
+    return <div className="error-fetch">Sorry.. something went wrong. try to reload the site or try again later</div>;
+  }
+
   return (
     <main className="grow">
       <div className="mx-auto max-w-screen-2xl px-3 sm:px-5">
         <h1 className="font-headings font-bold text-xl text-center text-primary my-10">Venue Manager</h1>
-        <ul className="flex flex-wrap justify-center mx-auto gap-5 max-w-2xl">List here</ul>
+        <ul className="flex flex-wrap justify-center mx-auto gap-5 max-w-2xl">
+          {data.map(venue => (
+            <ManagerVenueCard key={venue.id} venue={venue} />
+          ))}
+        </ul>
       </div>
     </main>
   );
