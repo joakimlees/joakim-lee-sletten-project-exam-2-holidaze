607cec8f2959f08d20ab63034c2161e0f96c976a
joakimlees <91544170+joakimlees@users.noreply.github.com>
2023-05-28 16:15:57 +0200

    working on create form

--- src/components/form/schema/createVenueSchema.js
+++ src/components/form/schema/createVenueSchema.js
@@ -3,16 +3,16 @@ import * as yup from "yup";
 export const createVenueSchema = yup
   .object({
     name: yup.string().min(3, "Name should be at least 3 characters.").max(20, "Name cannot be longer than 15 characters.").required("Please provide a name for your venue"),
-    description: yup.string().min(5, "Description should be at least 5 characters.").max(20, "Name cannot be longer than 15 characters."),
+    description: yup.string().min(5, "Description should be at least 5 characters."),
     media: yup.array().of(yup.string().url("Media must be a valid URL")),
     price: yup.number().required("Please provide a price for your venue"),
     maxGuests: yup.number().required("Please provide a guest limit for your venue"),
     rating: yup.number(),
     meta: yup.object().shape({
-      wifi: yup.boolean().optional().oneOf([true, false], "do your venue have wifi?"),
-      parking: yup.boolean().optional().oneOf([true, false], "do your venue include parking?"),
-      breakfast: yup.boolean().optional().oneOf([true, false], "do your venue offer included breakfast?"),
-      pets: yup.boolean().optional().oneOf([true, false], "is pets allowed in your venue?"),
+      wifi: yup.boolean().required("do your venue have wifi?"),
+      parking: yup.boolean().required("do your venue include parking?"),
+      breakfast: yup.boolean().required("do your venue offer included breakfast?"),
+      pets: yup.boolean().required("is pets allowed in your venue?"),
     }),
     location: yup.object().shape({
       address: yup.string().required("Please enter the address to your venue"),
--- src/components/pages/profile/manager/create/index.jsx
+++ src/components/pages/profile/manager/create/index.jsx
@@ -19,6 +19,34 @@ export function CreateVenue() {
         <h1 className="font-headings font-bold text-xl text-center text-primary my-10">Venue Create</h1>
         <form className="bg-light py-16 px-10 rounded-lg max-w-xs mx-auto mt-10 mb-20" method={"post"} action={""}>
           <FormField register={register} labelText="Venue Name" htmlFor="name" name="name" type="text" placeholder="Name of your venue" required={true} errorText={errors.name?.message} />
+
+          <FormField register={register} labelText="Description" htmlFor="description" name="description" type="textarea" placeholder="description of your venue or other details" required={true} errorText={errors.description?.message} />
+
+          <FormField register={register} labelText="Media image (URL)" htmlFor="media" name="media" type="url" placeholder="enter a image url" required={false} errorText={errors.media?.message} />
+
+          <FormField register={register} labelText="Price" htmlFor="price" name="price" type="number" placeholder="example: 199" required={true} errorText={errors.price?.message} />
+
+          <FormField register={register} labelText="Guest limit" htmlFor="maxGuests" name="maxGuests" type="number" placeholder="example: 4" required={true} errorText={errors.maxGuests?.message} />
+
+          <FormField register={register} labelText="Address" htmlFor="address" name="address" type="text" placeholder="venue street" required={true} errorText={errors.address?.message} />
+
+          <FormField register={register} labelText="City" htmlFor="city" name="city" type="text" placeholder="Oslo" required={false} errorText={errors.city?.message} />
+
+          <FormField register={register} labelText="Zip" htmlFor="zip" name="zip" type="text" placeholder="zip" required={false} errorText={errors.zip?.message} />
+
+          <FormField register={register} labelText="Country" htmlFor="country" name="country" type="text" placeholder="Norway" required={true} errorText={errors.country?.message} />
+
+          <FormField register={register} labelText="Continent" htmlFor="continent" name="continent" type="text" placeholder="Europe" required={true} errorText={errors.continent?.message} />
+
+          <div className="flex justify-between mb-8">
+            <FormField register={register} errorText={errors.wifi?.message} labelText="wifi" htmlFor="wifi" name="wifi" value={false} type="checkbox" required={true} />
+
+            <FormField register={register} errorText={errors.parking?.message} labelText="parking" htmlFor="parking" name="parking" value={false} type="checkbox" required={true} />
+
+            <FormField register={register} errorText={errors.breakfast?.message} labelText="breakfast" htmlFor="breakfast" name="breakfast" value={false} type="checkbox" required={true} />
+
+            <FormField register={register} errorText={errors.pets?.message} labelText="pets" htmlFor="breakfast" name="pets" value={false} type="checkbox" required={true} />
+          </div>
         </form>
       </div>
     </main>
