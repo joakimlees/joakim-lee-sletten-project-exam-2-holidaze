06e6ab5da3e83bc8b1cfc66a6ef89c062eb2720b
joakimlees <91544170+joakimlees@users.noreply.github.com>
2023-05-28 10:19:14 +0200

    working on edit bookings

--- src/components/pages/profile/bookings/edit/index.jsx
+++ src/components/pages/profile/bookings/edit/index.jsx
@@ -1,8 +1,9 @@
 import { BookingCalendar } from "../../../../calendar";
 import { useParams } from "react-router-dom";
 import { useAuthFetch } from "../../../../../api/auth/useAuthFetch";
-import { API_HOLIDAZE_URL } from "../../../../../api/constants";
+import { API_HOLIDAZE_URL, API_HOLIDAZE_VENUE_URL, BOOKINGS_OWNER } from "../../../../../api/constants";
 import { useEffect, useState } from "react";
+import { useFetch } from "../../../../../api/hooks/useFetch";
 
 export function EditUserBookings() {
   const { id } = useParams();
@@ -27,12 +28,17 @@ export function EditUserBookings() {
 
   const { venue } = bookingData || {};
   const { id: venueId } = venue || {};
+  const getVenueUrl = API_HOLIDAZE_VENUE_URL + "/" + venueId + BOOKINGS_OWNER;
 
-  if (bookingLoading) {
+  const { data: venueData, loading: venueLoading, error: venueError } = useFetch(getVenueUrl);
+
+  const { bookings } = venueData;
+
+  if (bookingLoading || venueLoading) {
     return <div className="loading-fetch">loading..................</div>;
   }
 
-  if (bookingError) {
+  if (bookingError || venueError) {
     return <div className="error-fetch">Sorry.. something went wrong. try to reload the site or try again later</div>;
   }
 
@@ -40,7 +46,7 @@ export function EditUserBookings() {
     <main className="grow">
       <div className="mx-auto max-w-screen-2xl px-3 sm:px-5">
         <h1 className="text-center mt-20 font-headings text-2xl text-dark">Edit booking</h1>
-        <BookingCalendar />
+        <BookingCalendar bookings={bookings} />
       </div>
     </main>
   );
